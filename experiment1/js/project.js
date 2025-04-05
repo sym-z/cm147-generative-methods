2fb0541aa62f83c1a63b4e1cc0f8030ba2a4ad54
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-04-05 14:36:50 -0700

    More content, punctuation fix, possibility for blank entries

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -2,12 +2,12 @@ const fillers = {
   intro: ["Come check out this new game! It's called", "Ugh I am so tired, I was up all night playing", "I have been thinking of picking up this game once it goes on sale, it's called", "Yo they just announced they're making"],
   preamble: ["The", "Legend of", "Adventures of", "Super"],
   starter: ["Bucket", "Frog", "Grumbo", "Cat-Man", "Block Bot", "Turtle", "Beetle"],
-  middle: ["Forest", "City", "Fantasy", "Treasure", "Story"],
+  middle: ["Forest", "City", "Fantasy", "Treasure", "Story", "Dungeon", "Incorporated", ""],
   connector: [":", "and the", "&"],
   descriptor: ["Curse of the", "Spirits of the", "Adventures in", "Revenge of the", "Tournament of", "Search for"],
   ending:["Souls", "Tennis", "Racing", "Party"],
-  postfix: ["64", "Ultra", "Reloaded", "Remix", "Deluxe", "2", "3", "World Tour"],
-  outro: ["! The final boss is crazy", "! It is free to play with a coupon.", "! The battle pass in it kind of doesn't make sense."]
+  postfix: ["64", "Ultra", "Reloaded", "Remix", "Deluxe", "2", "3", "World Tour", "3D", "Edition", ""],
+  outro: ["! The final boss is crazy.", "! It is free to play with a coupon.", "! The battle pass in it kind of doesn't make sense."]
 };
 
 const template = `$intro $preamble $starter $middle $connector $descriptor $ending $postfix$outro`;
