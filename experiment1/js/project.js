10cb29a990203c39b725ade1c88e5092289ef2c5
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-04-05 13:08:58 -0700

    Transfer of glitch project to portfolio site

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,40 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+const fillers = {
+  preamble: ["The", "Legend of", "Adventures of", "Super"],
+  starter: ["Bucket", "Frog", "Grumbo", "Cat-Man", "Block Bot", "Turtle", "Beetle"],
+  middle: ["Forest", "City", "Fantasy", "Treasure", "Story"],
+  connector: [":", "and the", "&"],
+  descriptor: ["Curse of the", "Spirits of the", "Adventures in", "Revenge of the", "Tournament of", "Search for"],
+  ending:["Souls", "Tennis", "Racing", "Party"],
+  postfix:["64", "Ultra", "Reloaded", "Remix", "Deluxe", "2", "3", "World Tour"],
+};
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const template = `Come check out this new game! It's called $preamble $starter $middle $connector $descriptor $ending $postfix!`;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
