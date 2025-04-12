200befb7fb2219c1cb6f681aaf53d7e15ded51dd
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-04-12 15:15:31 -0700

    foreground slope setup

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -32,6 +32,7 @@ var centerHorz, centerVert;
 let seed = 265;
 let mgSeed = 264;
 let bgSeed = 512;
+let fgSeed = 420;
 // setup() function is called once when the program starts
 function setup() {
   // place our canvas, making it fit our container
@@ -90,28 +91,28 @@ function drawMidground() {
   mgHeight = bgHeight;
   fill(LAND2);
   beginShape();
-    // TOP SIDE
-    for (let x = LAND_LOD; x < canvas.width + LAND_LOD; x += LAND_LOD) {
-      let y = mgHeight;
-      let noiseScale = 0.08;
-      let nx = x * noiseScale;
-      let noiseVal = MG_THICK * noise(nx);
-      let xPos = x;
-      if (x == LAND_LOD) {
-        xPos = 0;
-      }
-      let yPos = y + noiseVal;
-      vertex(xPos, yPos);
+  // TOP SIDE
+  for (let x = LAND_LOD; x < canvas.width + LAND_LOD; x += LAND_LOD) {
+    let y = mgHeight;
+    let noiseScale = 0.08;
+    let nx = x * noiseScale;
+    let noiseVal = MG_THICK * noise(nx);
+    let xPos = x;
+    if (x == LAND_LOD) {
+      xPos = 0;
     }
-    // BOTTOM SIDE
-    for (let x = canvas.width + LAND_LOD; x >= 0 - LAND_LOD; x -= LAND_LOD) {
-      let y = mgHeight + MG_THICK / 2;
-      let noiseScale = 0.01;
-      let nx = x * noiseScale;
-      let noiseVal = MG_THICK * noise(nx);
-      let yPos = y + noiseVal + MG_THICK;
-      vertex(x, yPos);
-    }
+    let yPos = y + noiseVal;
+    vertex(xPos, yPos);
+  }
+  // BOTTOM SIDE
+  for (let x = canvas.width + LAND_LOD; x >= 0 - LAND_LOD; x -= LAND_LOD) {
+    let y = mgHeight + MG_THICK / 2;
+    let noiseScale = 0.01;
+    let nx = x * noiseScale;
+    let noiseVal = MG_THICK * noise(nx);
+    let yPos = y + noiseVal + MG_THICK;
+    vertex(x, yPos);
+  }
   // vertex(0, mgHeight);
   // vertex(canvas.width, mgHeight);
   // vertex(canvas.width, mgHeight + MG_THICK);
@@ -119,14 +120,39 @@ function drawMidground() {
   endShape(CLOSE);
 }
 // Close coast
+const FG_SLOPE_FACTOR = 5;
 function drawForeground() {
-  fgHeight = random(mgHeight + MG_THICK + SEA_THICK, canvas.height - FG_THICK);
+  randomSeed(seed);
+  noiseSeed(fgSeed);
+  // fgHeight = random(mgHeight + MG_THICK + SEA_THICK, canvas.height - FG_THICK);
+  fgHeight = canvas.height - FG_THICK;
   fill(LAND3);
   beginShape();
-  vertex(0, fgHeight);
-  vertex(canvas.width, fgHeight);
-  vertex(canvas.width, canvas.height);
-  vertex(0, canvas.height);
+
+  // TOP SIDE
+  for (let x = LAND_LOD; x < canvas.width + LAND_LOD; x += LAND_LOD) {
+    let y = fgHeight;
+    let noiseScale = 0.08;
+    let nx = x * noiseScale;
+    let noiseVal = FG_THICK * noise(nx);
+    let xPos = x;
+    if (x == LAND_LOD) {
+      xPos = 0;
+    }
+    let yPos = y + noiseVal - x/FG_SLOPE_FACTOR;
+    vertex(xPos, yPos);
+  }
+  // BOTTOM SIDE
+  vertex(canvas.width + LAND_LOD,canvas.height+LAND_LOD);
+  vertex(-LAND_LOD,canvas.height+LAND_LOD);
+  // for (let x = canvas.width + LAND_LOD; x >= 0 - LAND_LOD; x -= LAND_LOD) {
+  //   let y = fgHeight + FG_THICK / 2;
+  //   let noiseScale = 0.01;
+  //   let nx = x * noiseScale;
+  //   let noiseVal = FG_THICK * noise(nx);
+  //   let yPos = y + noiseVal + FG_THICK;
+  //   vertex(x, yPos);
+  // }
   endShape(CLOSE);
 }
 const ROCK_COUNT_MIN = 5;
@@ -140,14 +166,14 @@ function drawRocks() {
   let numRocks = random(ROCK_COUNT_MIN, ROCK_COUNT_MAX);
   for (let i = 0; i < numRocks; i++) {
     let randomX = random(canvas.width);
-    let randomY = random(mgHeight + MG_THICK + ROCK_THICK_MAX*2, fgHeight);
+    let randomY = random(mgHeight + MG_THICK + ROCK_THICK_MAX * 2, fgHeight-fgHeight/FG_SLOPE_FACTOR);
     let randomW = random(ROCK_THICK_MIN, ROCK_THICK_MAX);
     let randomH = random(ROCK_THICK_MIN, ROCK_THICK_MAX);
     fill(ROCK_COLOR);
     let offset = sin(millis() / 800);
     offset = constrain(offset, -PI / 4, PI / 4);
     arc(randomX, randomY, randomW, randomH, PI - offset, 0 + offset, OPEN);
-    console.log(`drawing rock at ${randomX},${randomY}`);
+    // console.log(`drawing rock at ${randomX},${randomY}`);
   }
 }
 const FOAM_LEVEL = 180;
@@ -214,6 +240,7 @@ function mousePressed() {
   noiseSeed(millis());
   bgSeed = millis();
   mgSeed = millis() + frameCount;
+  fgSeed = millis() * random(0.2,0.9)
   seed = millis();
   paint();
 }
