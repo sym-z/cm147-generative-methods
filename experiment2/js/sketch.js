eb05999f687807222fea60bd0d305e2fc5e4d684
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-04-11 18:32:31 -0700

    sky drawn!

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -10,9 +10,10 @@
 const VALUE1 = 1;
 const VALUE2 = 2;
 
-const WATER = [0, 0, 255, 255];
+const SEA_LEVEL = 128;
+const WATER = [0.25, 0.66, 1, 1];
+const SKY = [0.4, 0.48, .6, 1];
 const LAND = [0, 255, 0, 255];
-const SKY = [255, 0, 0, 255];
 
 // Globals
 let canvasContainer;
@@ -30,27 +31,28 @@ function setup() {
 
   paint();
 }
-function paint(){
+function paint() {
+  drawSky();
   drawWater();
   drawRocks();
 }
 const ROCK_COUNT_MIN = 5;
 const ROCK_COUNT_MAX = 20;
-const ROCK_COLOR = [133, 75, 48, 180];
+const ROCK_COLOR = [70, 60, 52, 255];
 const ROCK_THICK_MIN = 20;
 const ROCK_THICK_MAX = 45;
 function drawRocks() {
   noStroke();
   randomSeed(millis());
-  let numRocks = random(ROCK_COUNT_MIN,ROCK_COUNT_MAX)
+  let numRocks = random(ROCK_COUNT_MIN, ROCK_COUNT_MAX);
   for (let i = 0; i < numRocks; i++) {
     let randomX = random(canvas.width);
-    let randomY = random(canvas.height);
+    let randomY = random(SEA_LEVEL, canvas.height);
     let randomW = random(ROCK_THICK_MIN, ROCK_THICK_MAX);
     let randomH = random(ROCK_THICK_MIN, ROCK_THICK_MAX);
     fill(ROCK_COLOR);
-    arc(randomX, randomY, randomW, randomH,PI, 0);
-    console.log(`drawing rock at ${randomX},${randomY}`)
+    arc(randomX, randomY, randomW, randomH, PI, 0);
+    console.log(`drawing rock at ${randomX},${randomY}`);
   }
 }
 function drawWater() {
@@ -62,11 +64,42 @@ function drawWater() {
   let tileW = 5;
   let tileH = 5;
   for (let x = 0; x < canvas.width; x += tileW) {
-    for (let y = 0; y < canvas.height; y += tileH) {
+    for (let y = SEA_LEVEL; y < canvas.height; y += tileH) {
       let nx = noiseScale * x;
       let ny = noiseScale * y;
       let c = 255 * noise(nx, ny) + brightness;
-      let color = [c / 4, c / 1.5, c, 255];
+      let color = [];
+      WATER.forEach((item, index) => {
+        color.push(c * item);
+      });
+      fill(color);
+      rect(x, y, x + tileW, y + tileH);
+    }
+  }
+}
+const CLOUD_BRIGHTNESS = 150;
+function drawSky() {
+  noStroke();
+  noiseSeed(millis());
+  let noiseLevel = 255;
+  let noiseScale = 0.055;
+  let brightness = 140;
+  let tileW = 2;
+  let tileH = 2;
+  for (let x = 0; x < canvas.width; x += tileW) {
+    for (let y = 0; y < canvas.height; y += tileH) {
+      let nx = noiseScale * x;
+      let ny = noiseScale * y;
+      let c = noiseLevel * noise(nx, ny); 
+      let color = [];
+      if (c < CLOUD_BRIGHTNESS) {
+        c+=brightness;
+        SKY.forEach((item, index) => {
+          color.push(c * item);
+        });
+      } else {
+        color =c+brightness  ;
+      }
       fill(color);
       rect(x, y, x + tileW, y + tileH);
     }
